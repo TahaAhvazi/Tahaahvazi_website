8f96636e9b63d5cb85cd7be45984fd657aaab69a
Taha <tahax.dev@gmail.com>
2022-09-13 12:54:30 +0430

    Make the website fully Responsive

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -1,4 +1,6 @@
-import 'package:TahaAhvazi/home_tablet.dart';
+import 'package:TahaAhvazi/responsive_Layouts/home_large_mobile.dart';
+import 'package:TahaAhvazi/responsive_Layouts/home_mobile.dart';
+import 'package:TahaAhvazi/responsive_Layouts/home_tablet.dart';
 import 'package:TahaAhvazi/responsive_Layouts/home_desktop.dart';
 import 'package:TahaAhvazi/responsive_Layouts/home_large_tablet.dart';
 import 'package:flutter/material.dart';
@@ -15,19 +17,18 @@ class Responsive extends StatelessWidget {
       body: LayoutBuilder(
         builder: (context, constraints) {
           if (constraints.maxWidth > 1628) {
-            return HomeScreenDesktop();
+            return const HomeScreenDesktop();
           }
           if (constraints.maxWidth > 1343 && constraints.maxWidth <= 1628) {
-            return HomeScreenLargeTablet();
+            return const HomeScreenLargeTablet();
           }
           if (constraints.maxWidth > 900 && constraints.maxWidth <= 1343) {
-            return HomeScreenTablet();
+            return const HomeScreenTablet();
           }
-          return Container(
-            color: Colors.white,
-            height: 200,
-            width: 500,
-          );
+          if (constraints.maxWidth > 450 && constraints.maxWidth <= 900) {
+            return const HomeScreenLargeMobile();
+          }
+          return const HomeScreenMobile();
         },
       ),
     );
